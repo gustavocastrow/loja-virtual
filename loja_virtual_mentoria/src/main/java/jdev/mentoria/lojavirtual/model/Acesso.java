8937fed0bc3b36d2b76c067d83c8ffeeb422df39
Gustavo Castro <gustavo.silva@revizia.com.br>
2024-07-09 10:06:00 -0300

    feat:2.3 implementação classe Acesso, Endereco, Pessoa, PessoaFisica, PessoaJuridica

---
+++ loja_virtual_mentoria/src/main/java/jdev/mentoria/lojavirtual/model/Acesso.java
@@ -0,0 +1,56 @@
+package jdev.mentoria.lojavirtual.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+
+@Entity
+@Table(name = "acesso")
+@SequenceGenerator(name = "seq_acesso", sequenceName = "seq_acesso", initialValue = 1, allocationSize = 1)
+public class Acesso implements GrantedAuthority {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_acesso")
+    private Long id;
+
+    @Column(nullable = false)
+    private String descricao; /*Acesso ex: ROLE_ADMIN ou ROLE_SECRETARIO */
+
+    @Override
+    public String getAuthority() {
+        return this.descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Acesso acesso = (Acesso) o;
+        return Objects.equals(id, acesso.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ loja_virtual_mentoria/src/main/java/jdev/mentoria/lojavirtual/model/PessoaJuridica.java
@@ -0,0 +1,73 @@
+package jdev.mentoria.lojavirtual.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pessoa_juridica")
+@PrimaryKeyJoinColumn(name = "id")
+public class PessoaJuridica extends Pessoa{
+    private static final long serialVersionUID = 1L;
+
+    private String cnpj;
+
+    private String inscEstatual;
+
+    private String inscMunicipal;
+
+    private String nomeFantasia;
+
+    private String razaoSocial;
+
+    private String categoria;
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getInscEstatual() {
+        return inscEstatual;
+    }
+
+    public void setInscEstatual(String inscEstatual) {
+        this.inscEstatual = inscEstatual;
+    }
+
+    public String getInscMunicipal() {
+        return inscMunicipal;
+    }
+
+    public void setInscMunicipal(String inscMunicipal) {
+        this.inscMunicipal = inscMunicipal;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+}
